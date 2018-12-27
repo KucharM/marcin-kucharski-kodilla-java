8553ff251ce6b2f10076ea72668353b2a11d8be1
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2018-12-27 14:23:06 +0100

    ex. 7.3

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -0,0 +1,28 @@
+package com.kodilla.stream.forumuser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Forum {
+
+    private final List<ForumUser> forumUserList = new ArrayList<>();
+
+    public Forum() {
+        forumUserList.add(new ForumUser(111, "jacek", 'M', 2000,
+                12, 20, 1));
+        forumUserList.add(new ForumUser(234, "janusz", 'M', 2000,
+                5, 3, 99));
+        forumUserList.add(new ForumUser(555, "marcin", 'M', 1988,
+                6, 15, 222));
+        forumUserList.add(new ForumUser(333, "jozek", 'M', 1990,
+                9, 22, 78));
+        forumUserList.add(new ForumUser(356, "romek", 'M', 1985,
+                2, 10, 332));
+        forumUserList.add(new ForumUser(278, "kasia", 'F', 1991,
+                11, 8, 100));
+    }
+
+    public List<ForumUser> getUserList() {
+        return new ArrayList<>(forumUserList);
+    }
+}
