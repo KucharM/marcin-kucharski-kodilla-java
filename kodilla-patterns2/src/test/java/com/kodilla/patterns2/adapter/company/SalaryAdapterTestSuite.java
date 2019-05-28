6e39f80c40b9d2876cf24fa3138a2799656f04b8
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2019-05-28 21:49:31 +0200

    24.1 facade

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/company/SalaryAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/company/SalaryAdapterTestSuite.java
@@ -1,6 +1,5 @@
 package com.kodilla.patterns2.adapter.company;
 
-import com.kodilla.patterns2.SalaryAdapter;
 import com.kodilla.patterns2.adapter.company.oldhrsystem.Workers;
 import org.junit.Assert;
 import org.junit.Test;
